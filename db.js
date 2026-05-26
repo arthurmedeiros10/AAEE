6d42c87343258b7b7b73f9dbb8559a5da241bd5e
arthur medeiros <medeiros.arthur13@escola.pr.gov.br>
2026-05-26 12:28:17 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,68 @@
+export const db = {
+    cursos: [
+      {
+        id:1,
+        nome: 'Administração',
+        cotas: {
+            2016: {
+                negro: { vagas: 3, candidatos: 10, candidatosPorVaga: 3.33 },
+                publica: { vagas: 12, candidatos: 189, candidatosPorVaga: 15.75 },
+                universal: { vagas: 15, candidatos: 219, candidatosPorVaga: 14.6 }
+            },
+            2017: {
+                negro: { vagas: 3, candidatos: 8, candidatosPorVaga: 2.66 },
+                publica: { vagas: 12, candidatos: 170, candidatosPorVaga: 14.16 },
+                universal: { vagas: 15, candidatos: 173, candidatosPorVaga: 11.53 }
+            },
+            2018: {
+                negro: { vagas: 3, candidatos: 4, candidatosPorVaga: 1.33 },
+                publica: { vagas: 12, candidatos: 131, candidatosPorVaga: 10.91 },
+                universal: { vagas: 15, candidatos: 188, candidatosPorVaga: 12.53 }
+            },
+            2019: {
+                negro: { vagas: 3, candidatos: 5, candidatosPorVaga: 1.66 },
+                publica: { vagas: 12, candidatos: 155, candidatosPorVaga: 12.91 },
+                universal: { vagas: 15, candidatos: 176, candidatosPorVaga: 11.73 }
+            },
+            2020: {
+                negro: { vagas: 'n/a', candidatos: 'n/a', candidatosPorVaga: 'n/a' },
+                publica: { vagas: 24, candidatos: 121, candidatosPorVaga: 5.04 },
+                universal: { vagas: 30, candidatos: 139, candidatosPorVaga: 4.63 }
+            },
+            2021: {
+                negro: { vagas: 'n/a', candidatos: 'n/a', candidatosPorVaga: 'n/a' },
+                publica: { vagas: 12, candidatos: 125, candidatosPorVaga: 10.41 },
+                universal: { vagas: 15, candidatos: 194, candidatosPorVaga: 12.93 }
+            },
+            2022: {
+                negro: { vagas: 3, candidatos: 6, candidatosPorVaga: 2.00},
+                publica_negro: { vagas: 6, candidatos: 8, candidatosPorVaga: 1.33},
+                publica: { vagas: 24, candidatos: 152, candidatosPorVaga: 6.33},
+                pcd: { vagas: 3, candidatos: 2, candidatosPorVaga: 0.66},
+                universal: { vagas: 24, candidatos: 130, candidatosPorVaga: 5.41}
+            },
+            2023: {
+                negro: { vagas: 3, candidatos: 6, candidatosPorVaga: 2.00},
+                publica_negro: { vagas: 6, candidatos: 7, candidatosPorVaga: 1.16},
+                publica: { vagas: 24, candidatos: 130, candidatosPorVaga: 5.41},
+                pcd: { vagas: 3, candidatos: 3, candidatosPorVaga: 1.00},
+                universal: { vagas: 24, candidatos: 86, candidatosPorVaga: 3.58}
+            },
+            2024: {
+                negro: { vagas: 3, candidatos: 3, candidatosPorVaga: 1.00},
+                publica_negro: { vagas: 6, candidatos: 8, candidatosPorVaga: 2.00},
+                publica: { vagas: 8, candidatos: 96, candidatosPorVaga: 12.00},
+                pcd: { vagas: 3, candidatos: 2, candidatosPorVaga: 0.66},
+                universal: { vagas: 24, candidatos: 101, candidatosPorVaga: 4.20}
+            },
+            2025: {
+                negro: { vagas: 3, candidatos: 5, candidatosPorVaga: 1.66},
+                publica_negro: { vagas: 6, candidatos: 14, candidatosPorVaga: 2.33},
+                publica: { vagas: 8, candidatos: 99, candidatosPorVaga: 12.37},
+                pcd: { vagas: 3, candidatos: 3, candidatosPorVaga: 1.00},
+                universal: { vagas: 24, candidatos: 91, candidatosPorVaga: 3.79}
+            }
+        }
+      }
+    ]
+}
